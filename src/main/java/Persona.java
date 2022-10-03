bf1d02cdb56f7fa0135601862284a466e8c7349e
JoseAntonioMardonez <j.mardonez01@ufromail.cl>
2022-10-03 15:41:36 -0300

    Se agregan Atributos y Metodos básicos

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,2 +1,21 @@
 public class Persona {
+    //Atributos
+    private String nombre, apellido;
+    private int edad;
+    //Constructor
+    public Persona ( String nombre, String apellido, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.apellido = apellido;
+    }
+    //Metodos
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellido(){
+        return apellido;
+    }
+    public int getEdad() {
+        return edad;
+    }
 }
--- src/main/java/Registro.java
+++ src/main/java/Registro.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Registro {
+    //Atributos
+    private String nombre;
+    private List<Persona> pacientes;
+    //Constructor
+    public Registro(String nombre) {
+        this.nombre = nombre;
+        this.pacientes = new ArrayList<Persona>();
+    }
+    //Metodos
+    public Persona agregarPaciente(String nombre, String apellido, int edad){
+        Persona pacienteNuevo = new Persona(nombre, apellido, edad);
+        this.pacientes.add(pacienteNuevo);
+        return pacienteNuevo;
+    }
+    public Boolean eliminarPaciente(String nombre, String apellido){
+        for(Persona paciente : this.pacientes){
+            if(paciente.getNombre().equals(nombre) && paciente.getApellido().equals(apellido)){
+                this.pacientes.remove(paciente);
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+
 }
